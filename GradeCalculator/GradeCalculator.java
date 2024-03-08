4f084662f1f4a0035364168396a6be890b831e07
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-03-08 08:49:45 -0700

    update example + use try-with-resource

--- GradeCalculator/GradeCalculator.java
+++ GradeCalculator/GradeCalculator.java
@@ -4,34 +4,34 @@ public class GradeCalculator {
 
     public static void main(String[] args) {
         final int GRADE_ENTRIES = 10;
-        Scanner scanner = new Scanner(System.in);
         double currentGrade = 0;
         double totalGrade = 0;
         double avgGrade = 0;
         double maxGrade = Double.MIN_VALUE;
         double minGrade = Double.MAX_VALUE;
 
-        for (int i = 0; i < GRADE_ENTRIES; i++) {
-            System.out.println("Grade Entry #" + (i + 1));
-            while (!scanner.hasNextDouble()) {
-                System.out.println("Invalid input, please try again! use a floating-point number to make an entry.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (int i = 0; i < GRADE_ENTRIES; i++) {
                 System.out.println("Grade Entry #" + (i + 1));
-                scanner.next();
-            }
+                while (!scanner.hasNextDouble()) {
+                    System.out
+                            .println("Invalid input, please try again! use a floating-point number to make an entry.");
+                    System.out.println("Grade Entry #" + (i + 1));
+                    scanner.next();
+                }
 
-            currentGrade = scanner.nextDouble();
-            totalGrade += currentGrade;
+                currentGrade = scanner.nextDouble();
+                totalGrade += currentGrade;
 
-            if (currentGrade > maxGrade) {
-                maxGrade = currentGrade;
-            }
+                if (currentGrade > maxGrade) {
+                    maxGrade = currentGrade;
+                }
 
-            if (currentGrade < minGrade) {
-                minGrade = currentGrade;
+                if (currentGrade < minGrade) {
+                    minGrade = currentGrade;
+                }
             }
         }
-
-        scanner.close();
 
         avgGrade = totalGrade / GRADE_ENTRIES;
         System.out.println("Statistics:");
